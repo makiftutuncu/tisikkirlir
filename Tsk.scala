6c278dc05c3f5ccc21bd51a5e17d53903c146475
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2015-04-23 00:18:09 +0300

    Fixed alphabet, I forgot half of the letters, wow! :)

--- Tsk.scala
+++ Tsk.scala
@@ -1,5 +1,5 @@
 object Tsk {
   implicit class ToTsk(s: String) {
-    def tsk = s.replaceAll("[eıüö]", "i").replaceAll("[EIÜÖ]", "İ")
+    def tsk = s.replaceAll("[aeıoöuü]", "i").replaceAll("[AEIOÖUÜ]", "İ")
   }
 }
